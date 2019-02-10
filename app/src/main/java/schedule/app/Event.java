7487a435f986d9f7332b3b68db6478f4734853e4
Saida Jaime <saidaj12@gmail.com>
2019-02-09 23:27:41 -0500

    Added Event.java

---
+++ app/src/main/java/schedule/app/Event.java
@@ -0,0 +1,29 @@
+package schedule.app;
+
+public class Event {
+    private int time;
+    private int start;
+    private int stop;
+    private String name;
+    private String category;
+    private boolean split;
+
+    public Event(String name, String category, int time, boolean split) {
+        this.time = time;
+        this.start = 0;
+        this.stop = 0;
+        this.name = name;
+        this.category = category;
+        this.split = split;
+    }
+
+    public int getTime() { return time; }
+    public int getStart() { return start; }
+    public int getStop() { return stop; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public boolean getSplit() { return split; }
+
+    public void setStart(int start) { this.start = start; }
+    public void setStop(int stop) { this.stop = stop; }
+}
